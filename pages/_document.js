86d78153c19cd909e214423f9ee26549fad7c29a
ferdousr3 <ferdousr1992@gmail.com>
2021-09-06 01:53:28 +0600

    all

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
-import { ServerStyleSheets } from '@material-ui/core/styles';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
-export default class MyDocument extends Document{
-  render()
-  {
+
+
+export default class MyDocument extends Document {
+  render() {
     return (
       <Html lang="en">
         <Head>
@@ -22,12 +23,10 @@ export default class MyDocument extends Document{
   }
 }
 
-MyDocument.getInitialProps = async (ctx) =>
-{
+MyDocument.getInitialProps = async (ctx) => {
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
-  ctx.renderPage = () =>
-  {
+  ctx.renderPage = () => {
     return originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
