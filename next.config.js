6004ffb46212a9a76467388a5147f520b14945b4
ferdousr3 <ferdousr1992@gmail.com>
2021-09-06 02:14:34 +0600

    all

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   reactStrictMode: true,
 }
-module.exports = {
-  eslint: {
-    dirs: [ 'pages', 'utils' ], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
-}
