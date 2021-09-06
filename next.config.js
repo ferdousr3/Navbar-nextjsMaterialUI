cb71755d0e0b44df08585f947e23ff4e0883b0a7
ferdousr3 <ferdousr1992@gmail.com>
2021-09-06 16:16:31 +0600

    all

--- next.config.js
+++ next.config.js
@@ -6,3 +6,9 @@ module.exports = {
 //     dirs: [ 'pages', 'utils' ], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
 //   },
 // }
+module.exports = {
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: true,
+  target: "experimental-serverless-trace",
+}
